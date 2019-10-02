9e308f9954bac7de335f60b10cd7d6272a034ba0
katsukaree <katsukaree@protonmail.ch>
2019-10-02 20:46:27 +1300

    Read file

--- aes.c
+++ aes.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stlib.h>
 
 #define   BLOCKSIZE 128
+#define   KEYLENGTH 128
+#define   ROUNDS    10
 
-int main() {
-	return 0;
+int main(int argc, char *argv[]) {
+    char buffer[16];
+    char state[4][4];
+
+    FILE *input_file = fopen(argv[1], "rb");
+    
+    if (fp == NULL) {
+        fprintf(stderr, "cannot open input file\n");
+        return 1;
+    }
+ 
+    return 0;
+   
 }
