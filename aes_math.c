f24adc143e898439c8cc72f4ef29d107b3104a9b
katsukaree <katsukaree@protonmail.ch>
2019-10-18 21:11:31 +1300

    Update math in a completely untested manner

--- aes_math.c
+++ aes_math.c
@@ -4,6 +4,9 @@
 
 #define  MULT_IRR_POLY 283; 
 
+unsigned char multiplication(unsigned char a, unsigned char b);
+unsigned char gf_modulo(unsigned char c, unsigned char d);
+
 unsigned char multiplication(unsigned char multiplicand, unsigned char multiplier)
 {
     unsigned char product;
@@ -12,6 +15,10 @@ unsigned char multiplication(unsigned char multiplicand, unsigned char multiplie
     if (multiplier==1)
     {
         product = multiplicand;
+    }
+    else if (multiplicand == 1)
+    {
+        product = multiplier;
     }
     else
     {
@@ -22,4 +29,35 @@ unsigned char multiplication(unsigned char multiplicand, unsigned char multiplie
             multiplier = multiplier ^ (multiplier * (2 * bitlength));
         }
     }
+    return product;
+}
+
+unsigned char gf_modulo(unsigned char dividend, unsigned char divisor)
+{
+    unsigned char modulus;
+    unsigned int dividend_length, divisor_length;
+
+    dividend_length = (int) (log2(dividend) - 1);
+    divisor_length = (int) (log2(divisor) - 1);
+
+    modulus = dividend;
+
+    while (dividend_length >= divisor_length)
+    {
+        modulus = modulus ^ (divisor * (2 * (dividend_length - divisor_length)));
+        dividend_length = (int) (log2(dividend) - 1);
+        divisor_length = (int) (log2(divisor) - 1);
+    }
+
+    return modulus;
+}
+
+unsigned char gf_multiplication(unsigned char multiplicand, unsigned char multiplier)
+{
+    unsigned char product;
+
+    product = multiplication(multiplicand, multiplier);
+    product = gf_modulo(product, MULT_IRR_POLY);
+
+    return product;
 }
