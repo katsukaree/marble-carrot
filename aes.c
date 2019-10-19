15be9eedf9f65a8f2bc692287ee0d3f457b27b49
katsukaree <katsukaree@protonmail.ch>
2019-10-19 17:31:15 +1300

    Tidying up for the day

--- aes.c
+++ aes.c
@@ -8,26 +8,38 @@
 
 int main(int argc, char *argv[]) {
     unsigned char *buffer;
-    FILE *input_file = fopen(argv[1], "rb");
-    
-    // Check for file argument
-    if (argc != 2) {
-    	fprintf(stderr, "Usage: ./aes.out INPUT_FILE\n");
-	return 1;
+
+    // Check for input and key file arguments
+    if (argc != 3) 
+    {
+    	fprintf(stderr, "Usage: ./aes.out INPUT_FILE KEY_FILE\n");
+    	return 1;
     }
-    
-    // Check for file. If not, throw error
-    if (!input_file) {
-        fprintf(stderr, "Cannot find file %s\n", argv[1]);
+
+    FILE *input_file = fopen(argv[1], "rb");
+    FILE *key_file = fopen(argv[2], "rb");   
+
+    // Check for input file. If not, throw error
+    if (!input_file) 
+    {
+        fprintf(stderr, "Cannot find input file %s\n", argv[1]);
         return 1;
     }
 
-    // Read file into buffer
+    // Check for key file. If not, throw error
+    if (!key_file)
+    {
+        fprintf(stderr, "Cannot find key file %s\n", argv[2]);
+        return 1;
+    }
+
+    // Read input file into buffer
     fseek(input_file, 0, SEEK_END);
     unsigned long file_len = ftell(input_file);
     fseek(input_file, 0, SEEK_SET);
 
     buffer=(char *)malloc(file_len+1);
+
     if (!buffer)
     {
         fprintf(stderr, "Memory error!");
@@ -52,6 +64,9 @@ int main(int argc, char *argv[]) {
             k++;
         }
     }
+
+    unsigned char product = gf_multiplication(87, 19);
+    printf("0x%x\n", product);
 
     return 0;   
 }
